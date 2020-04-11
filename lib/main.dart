702cb066a6e09a9cab53fa2c12fa8b6b638fdb7d
Miguel Yann <tsambamiguelito@gmail.com>
2020-04-12 00:06:15 +0200

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:quizz_demo/quiz.dart';
+import 'package:quizz_demo/result.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  static const List<Map<String, dynamic>> _questionsAnswers = [
+    {
+      "question": "What is your favorite language?",
+      "answers": ['Java', 'C#', 'Python', 'JavaScript']
+    },
+    {
+      "question": "Which free time you have per day?",
+      "answers": ['1 hour', '2 hours', '3hours', "I'm always free"]
+    },
+    {
+      "question": "Do you have a personal computer?",
+      "answers": ["Yes", "No"]
+    },
+  ];
+  static int _index = 0;
+  static const _TEXT_RESULT = 'Thank You';
+
+  void handlerQuestionsAnswers() {
+    if (_index < _questionsAnswers.length) {
+      setState(() {
+        _index++;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+          appBar: AppBar(
+            title: Text('Process Learn'),
+            centerTitle: true,
+          ),
+          body: _index < _questionsAnswers.length
+              ? Quiz(
+                  question: _questionsAnswers[_index]['question'],
+                  answers: _questionsAnswers[_index]['answers'],
+                  answerQuestion: handlerQuestionsAnswers)
+              : Result(result: _TEXT_RESULT)),
+    );
+  }
+}
---
+++ lib/quiz.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+import 'answer.dart';
+import 'question.dart';
+
+class Quiz extends StatelessWidget {
+  final String question;
+  final List<String> answers;
+  final Function answerQuestion;
+
+  const Quiz(
+      {@required this.question,
+      @required this.answers,
+      @required this.answerQuestion});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Question(question),
+        ...answers.map((answer) => Answer(answerQuestion, answer)).toList()
+      ],
+    );
+  }
+}
